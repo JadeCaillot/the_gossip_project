6a9d2b96fe4bae8a33d9a48a06613efe0b186e86
JadeCaillot <jadeca@wanadoo.fr>
2020-02-13 14:07:14 +0100

    création utilisateur

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,48 @@
 class UsersController < ApplicationController
+  def index 
+    @users=User.all
+  end 
+
   def show
-    @user=User.find(params[:id]) #pour permettre au fichier html show_auteur de reconnaitre la variable
+    @user=User.find(params[:id]) 
   end
+
+  def new
+    @user=User.new
+  end
+
+  def create
+    @user = User.new(first_name: params[:first_name], last_name: params[:last_name], age: params[:age], email: params[:email], description: params[:description], city:City.last, password: params[:password])
+    if @user.save
+      redirect_to root_path
+    else
+      puts "Ca n'a pas fonctionné désolé"
+      render :new
+    end
+  end
+
+  def edit
+    @user=User.find(params[:id])
+  end 
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end 
+  end
+
+  def destroy
+    @user=User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end 
+
+  private
+  def user_params
+    user_params=params.require(:user).permit(:first_name, :last_name, :age, :email, :description, :city, :password)
+  end 
+  
 end
